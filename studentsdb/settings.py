ad87e8b4ecf2ff11bbb4b0f8e166baf3e72ff1e4
Denis Rakhuba <denis.rakhuba@gmail.com>
2016-01-26 20:29:46 +0200

    templates

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -28,13 +28,14 @@ ALLOWED_HOSTS = []
 
 from django.conf import global_settings
 
-TEMPLATE_CONTEXT_PROCESSORS = \
+''' TEMPLATE_CONTEXT_PROCESSORS = \
 	global_settings.TEMPLATE_CONTEXT_PROCESSORS + [
 	'django.core.context_processors.request',
 	'studentsdb.context_processors.students_proc',
-]
+] '''
 
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
 
 # Application definition
 
@@ -72,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+		'studentsdb.context_processors.students_proc',
             ],
         },
     },
