1d36965b6b5237c605cc5ad9e46f6436f7a2cf13
Denis Rakhuba <denis.rakhuba@gmail.com>
2016-02-04 17:28:55 +0200

    chapt. 7

--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Students urls
     url(r'^$', 'students.views.students.students_list', name='home'),
@@ -31,3 +32,9 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 )
 
+from .settings import MEDIA_ROOT, DEBUG
+# serve files from media folder
+if DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': MEDIA_ROOT}))
